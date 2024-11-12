e2eb3af40e63752ecfbfddd1fc20648fdfcfabbb
Adrian <adrianglos117@googlemail.com>
2024-11-12 15:24:43 +0100

    Check if the libraries

--- R/CalibrationPlot.R
+++ R/CalibrationPlot.R
@@ -43,12 +43,12 @@
 calibrationplot <- function(learners, task, bins = 10,
                             smooth = FALSE, CI = FALSE, rug = FALSE) {
 
-  if (!requireNamespace("dplyr", quietly = TRUE)) {
-    stop("The 'dplyr' package is required to use this function. Please load the libray and try it again")
+  if (!require("dplyr")) {
+    stop("The 'dplyr' package is required to use this function. Please install the libray and try it again")
   }
 
-  if (!requireNamespace("ggplot2", quietly = TRUE)) {
-    stop("The 'ggplot2' package is required to use this function.Please load the libray and try it again")
+  if (!require("ggplot2")) {
+    stop("The 'ggplot2' package is required to use this function. Please install the libray and try it again")
   }
 
   all_data <- data.frame()
@@ -68,22 +68,22 @@ calibrationplot <- function(learners, task, bins = 10,
   dummy_line <- data.frame(mean_res = c(0, 1), mean_truth = c(0, 1), learner_id = "Perfectly Calibrated")
 
   p <- ggplot2::ggplot() +
-    ggplot2::geom_line(data = dummy_line, aes(x = mean_res, y = mean_truth, color = learner_id), linetype = "dashed", show.legend = TRUE) +
-    ggplot2::theme_minimal() +
-    ggplot2::xlim(0, 1) +
-    ggplot2::ylim(0, 1) +
-    ggplot2::labs(x = "Mean Prediction", y = "Mean Truth", color = "Learner") +
-    ggplot2::scale_color_manual(values = c("Perfectly Calibrated" = "black", setNames(scales::hue_pal()(length(unique(all_data$learner_id))), unique(all_data$learner_id)))) +
-    ggplot2::theme(legend.position = c(0.85, 0.25)) +
-    ggplot2::theme(legend.background = element_rect(color = "black", size = 0.5)) +
-    ggplot2::ggtitle("Reliability Curve") +
-    ggplot2::theme(plot.title = element_text(hjust = 0.5, size = 20))
+    geom_line(data = dummy_line, aes(x = mean_res, y = mean_truth, color = learner_id), linetype = "dashed", show.legend = TRUE) +
+    theme_minimal() +
+    xlim(0, 1) +
+    ylim(0, 1) +
+    labs(x = "Mean Prediction", y = "Mean Truth", color = "Learner") +
+    scale_color_manual(values = c("Perfectly Calibrated" = "black", setNames(scales::hue_pal()(length(unique(all_data$learner_id))), unique(all_data$learner_id)))) +
+    theme(legend.position = c(0.85, 0.25)) +
+    theme(legend.background = element_rect(color = "black", size = 0.5)) +
+    ggtitle("Reliability Curve") +
+    theme(plot.title = element_text(hjust = 0.5, size = 20))
 
   if (smooth) {
-    p <- p + ggplot2::geom_smooth(data = all_data, aes(x = mean_res, y = mean_truth, color = learner_id), method = "loess", se = CI)
+    p <- p + geom_smooth(data = all_data, aes(x = mean_res, y = mean_truth, color = learner_id), method = "loess", se = CI)
   } else {
-    p <- p + ggplot2::geom_point(data = all_data, aes(x = mean_res, y = mean_truth, color = learner_id)) +
-      ggplot2::geom_line(data = all_data, aes(x = mean_res, y = mean_truth, color = learner_id))
+    p <- p + geom_point(data = all_data, aes(x = mean_res, y = mean_truth, color = learner_id)) +
+      geom_line(data = all_data, aes(x = mean_res, y = mean_truth, color = learner_id))
   }
 
   return(p)

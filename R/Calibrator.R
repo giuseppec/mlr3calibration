2dddaa58d7ab8463dbf2228428ad6e8dbe68fc26
eli <es13meijer@gmail.com>
2025-06-24 18:31:12 +0200

    added CalibratorIsotonic, corrected tests

--- R/Calibrator.R
+++ R/Calibrator.R
@@ -12,13 +12,15 @@ Calibrator <- R6::R6Class("Calibrator",
                       public = list(
                         #' @description Abstract initializer
                         #' @param calibration_data A data.table with columns "truth" and "response"
-                        initialize = function(calibration_data) {
+                        #' @param task test set of the task, on which the original learner was trained
+                        initialize = function(calibration_data, task) {
                           stop("This is an abstract class. Use a subclass.")
                         },
                         #' @description Abstract prediction method
                         #' @param calibration_data A data.table with columns "truth" and "response"
+                        #' @param task test set of the task, on which the original learner was trained
                         #' @return A numeric vector of predicted probabilities
-                        predict = function(calibration_data) {
+                        predict = function(calibration_data, task) {
                           stop("predict() must be implemented in the subclass.")
                         }
                       )
--- tests/testthat/test-Calibrator.R
+++ tests/testthat/test-Calibrator.R
@@ -19,8 +19,37 @@ test_that("CalibratorPlatt", {
   calibration_data$response = as.numeric(calibration_data$response)
 
   # Create Calibrator
-  calib_platt = CalibratorPlatt$new(calibration_data)
+  calib_platt = CalibratorPlatt$new(calibration_data, task_test)
   # Predict with Calibrator
-  pred_calibrated = calib_platt$predict(calibration_data)
-  checkmate::expect_numeric(mean(pred_calibrated$prob.M))
+  pred_calibrated = calib_platt$predict(calibration_data, task_test)
+  checkmate::expect_numeric(mean(pred_calibrated$prob[,1]))
+})
+
+test_that("CalibratorIsotonic", {
+  data("Sonar", package = "mlbench")
+  task = as_task_classif(Sonar, target = "Class", positive = "M")
+  learner <- lrn("classif.rpart", predict_type = "prob")
+  splits = partition(task)
+  task_train = task$clone()$filter(splits$train)
+  task_test = task$clone()$filter(splits$test)
+
+  positive = task$positive
+  learner$train(task_train)
+  preds = learner$predict(task_test)
+
+  pred_data = as.data.table(preds)
+  calibration_data = data.table(truth = pred_data$truth,
+                                response = with(pred_data,
+                                                get(paste0("prob.", positive))))
+
+  colnames(calibration_data) = c("truth", "response")
+  calibration_data$response = as.numeric(calibration_data$response)
+  print("response:")
+  print(length(calibration_data$response))
+
+  # Create Calibrator
+  calib_isotonic = CalibratorIsotonic$new(calibration_data, task_test)
+  # Predict with Calibrator
+  pred_calibrated = calib_isotonic$predict(calibration_data, task_test)
+  checkmate::expect_numeric(mean(pred_calibrated$prob[,1]))
 })

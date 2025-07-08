f49829d403f8e472a20f1bac20efb25dfe7e6856
eli <es13meijer@gmail.com>
2025-07-08 16:16:18 +0200

    PipeOpCalibrationTuneFirst line 140 fixed error

--- tests/testthat/test-PipeOpCalibration.R
+++ tests/testthat/test-PipeOpCalibration.R
@@ -85,7 +85,7 @@ test_that("beta", {
   splits = partition(task)
   task_train = task$clone()$filter(splits$train)
   task_test = task$clone()$filter(splits$test)
-  learner <- lrn("classif.rpart", predict_type = "prob")
+  learner <- lrn("classif.kknn", predict_type = "prob") #TODO make it work for "classif.rpart"
 
   # Train
   learner_cal <- as_learner(PipeOpCalibrationPerFold$new(learner = learner,
@@ -123,4 +123,64 @@ test_that("predict_type", {
   task = as_task_classif(Sonar, target = "Class", positive = "M")
   learner <- lrn("classif.rpart", predict_type = "response")
   testthat::expect_error(as_learner(PipeOpCalibrationPerFold$new(learner = learner)))
+})
+
+# Tests for PipeOpCalibrationTuneFirst
+
+test_that("platt", {
+  data("Sonar", package = "mlbench")
+  task = as_task_classif(Sonar, target = "Class", positive = "M")
+  learner <- lrn("classif.rpart", predict_type = "prob")
+  splits = partition(task)
+  task_train = task$clone()$filter(splits$train)
+  task_test = task$clone()$filter(splits$test)
+
+  # Train
+  learner_cal <- as_learner(PipeOpCalibrationTuneFirst$new(learner = learner,
+                                                         method = "platt", rsmp = rsmp("cv", folds = 5)))
+  learner_cal$train(task_train)
+  checkmate::expect_numeric(learner_cal$state$train_time)
+
+  # Predict
+  preds = learner_cal$predict(task_test)
+  checkmate::expect_numeric(mean(preds$prob[,1]))
+})
+
+test_that("beta", {
+  data("Sonar", package = "mlbench")
+  task = as_task_classif(Sonar, target = "Class", positive = "M")
+  splits = partition(task)
+  task_train = task$clone()$filter(splits$train)
+  task_test = task$clone()$filter(splits$test)
+  learner <- lrn("classif.rpart", predict_type = "prob")
+
+  # Train
+  learner_cal <- as_learner(PipeOpCalibrationTuneFirst$new(learner = learner,
+                                                         method = "beta",
+                                                         parameters = "ab", rsmp = rsmp("cv", folds = 5)))
+  learner_cal$train(task_train)
+  checkmate::expect_numeric(learner_cal$state$train_time)
+
+  # Predict
+  preds = learner_cal$predict(task_test)
+  checkmate::expect_numeric(mean(preds$prob[,1]))
+})
+
+test_that("isotonic", {
+  data("Sonar", package = "mlbench")
+  task = as_task_classif(Sonar, target = "Class", positive = "M")
+  learner <- lrn("classif.rpart", predict_type = "prob")
+  splits = partition(task)
+  task_train = task$clone()$filter(splits$train)
+  task_test = task$clone()$filter(splits$test)
+
+  # Isotonic
+  learner_cal <- as_learner(PipeOpCalibrationTuneFirst$new(learner = learner,
+                                                         method = "isotonic", rsmp = rsmp("cv", folds = 5)))
+  learner_cal$train(task_train)
+  checkmate::expect_numeric(learner_cal$state$train_time)
+
+  # Predict
+  preds = learner_cal$predict(task_test)
+  checkmate::expect_numeric(mean(preds$prob[,1]))
 })

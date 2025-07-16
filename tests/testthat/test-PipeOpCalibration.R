62e96cfb7ed11672aebbcffe94ccc388f0eb56e5
eli <es13meijer@gmail.com>
2025-07-16 09:56:26 +0200

    created test for PipeOpTuneFirstOOF with working autotuner

--- tests/testthat/test-PipeOpCalibration.R
+++ tests/testthat/test-PipeOpCalibration.R
@@ -211,3 +211,80 @@ test_that("isotonic", {
   preds = learner_cal$predict(task_test)
   checkmate::expect_numeric(mean(preds$prob[,1]))
 })
+
+# Tests for PipeOpCalibrationTuneFirstOOF
+
+test_that("platt", {
+  data("Sonar", package = "mlbench")
+  task = as_task_classif(Sonar, target = "Class", positive = "M")
+  learner <- lrn("classif.rpart", predict_type = "prob")
+
+  # Define tuner
+  tuner = mlr3tuning::tnr("grid_search", resolution = 10)
+
+  # AutoTuner
+  at = mlr3tuning::AutoTuner$new(
+    learner = learner,
+    resampling = rsmp("cv", folds = 5),
+    measure = msr("classif.acc"),
+    search_space = paradox::ps(
+      cp = paradox::p_dbl(0.001,0.1)
+    ),
+    terminator = trm("evals", n_evals = 10),
+    tuner = tuner
+  )
+  splits = partition(task)
+  task_train = task$clone()$filter(splits$train)
+  task_test = task$clone()$filter(splits$test)
+
+  # Train
+  learner_cal <- as_learner(PipeOpCalibrationTuneFirstOOF$new(learner = at,
+                                                           method = "platt", rsmp = rsmp("cv", folds = 5)))
+  learner_cal$train(task_train)
+  checkmate::expect_numeric(learner_cal$state$train_time)
+
+  # Predict
+  preds = learner_cal$predict(task_test)
+  checkmate::expect_numeric(mean(preds$prob[,1]))
+})
+
+test_that("beta", {
+  data("Sonar", package = "mlbench")
+  task = as_task_classif(Sonar, target = "Class", positive = "M")
+  splits = partition(task)
+  task_train = task$clone()$filter(splits$train)
+  task_test = task$clone()$filter(splits$test)
+  learner <- lrn("classif.rpart", predict_type = "prob")
+  at <- mlr3tuning::auto_tuner(learner, rsmp("cv", folds = 5), msr("classif.acc"))
+
+  # Train
+  learner_cal <- as_learner(PipeOpCalibrationTuneFirstOOF$new(learner = at,
+                                                           method = "beta",
+                                                           parameters = "ab", rsmp = rsmp("cv", folds = 5)))
+  learner_cal$train(task_train)
+  checkmate::expect_numeric(learner_cal$state$train_time)
+
+  # Predict
+  preds = learner_cal$predict(task_test)
+  checkmate::expect_numeric(mean(preds$prob[,1]))
+})
+
+test_that("isotonic", {
+  data("Sonar", package = "mlbench")
+  task = as_task_classif(Sonar, target = "Class", positive = "M")
+  learner <- lrn("classif.rpart", predict_type = "prob")
+  at <- mlr3tuning::auto_tuner(learner, rsmp("cv", folds = 5), msr("classif.acc"))
+  splits = partition(task)
+  task_train = task$clone()$filter(splits$train)
+  task_test = task$clone()$filter(splits$test)
+
+  # Isotonic
+  learner_cal <- as_learner(PipeOpCalibrationTuneFirstOOF$new(learner = at,
+                                                           method = "isotonic", rsmp = rsmp("cv", folds = 5)))
+  learner_cal$train(task_train)
+  checkmate::expect_numeric(learner_cal$state$train_time)
+
+  # Predict
+  preds = learner_cal$predict(task_test)
+  checkmate::expect_numeric(mean(preds$prob[,1]))
+})
